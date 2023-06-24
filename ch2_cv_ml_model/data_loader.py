72a65008bbc57885f817202a8106c524119fd088
deasadiqbal <asad95298@gmail.com>
2023-06-24 22:34:51 +0500

    add new feature

--- ch2_cv_ml_model/data_loader.py
+++ ch2_cv_ml_model/data_loader.py
@@ -1,3 +1,25 @@
-class Dataloader:
-    def __init__(self) -> None:
-        pass
+#reading the data
+import tensorflow as tf
+def load_and_decode(filename, reshape_dim):
+    img = tf.io.read_file(filename)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    return tf.image.resize(img, reshape_dim)
+
+
+file_name = "E:\\computer vision\\practical-cv\\dataset\\train"
+#vizulize the data
+import matplotlib.pyplot as plt
+def show_img(filename):
+    img = load_and_decode(filename, [256, 256])
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+
+print(show_img(file_name))
+
+tulips = tf.io.gfile.glob(
+ "gs://cloud-ml-data/img/flower_photos/tulips/*.jpg")
+for i in range(5):
+    show_img(tulips[i])
