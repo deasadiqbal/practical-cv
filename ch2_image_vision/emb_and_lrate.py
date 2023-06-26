83938a7e60d8eb3aae695aa677538978050af13e
deasadiqbal <asad95298@gmail.com>
2023-06-26 15:07:03 +0500

    adding some code

--- ch2_image_vision/emb_and_lrate.py
+++ ch2_image_vision/emb_and_lrate.py
@@ -72,3 +72,70 @@ def train_and_evaluate(batch_size = 32,
   history = model.fit(train_dataset, validation_data=eval_dataset, epochs=5)
   training_plot(['loss', 'accuracy'], history)
   return model
+
+
+#pretrained model in keras
+import tensorflow as tf
+from keras_adamw import AdamW
+
+preTrained_model = tf.keras.applications.MobileNetV2(input_shape=[244,244,3],
+                                                     weights='Imagenet',
+                                                     include_top=False)
+preTrained_model.trainable = False
+
+model = tf.keras.Sequential([
+  # convert image format from int [0,255]
+  # to the format expected by this model
+  tf.keras.layers.Lambda(
+    lambda data: tf.keras.applications.mobilenet.preprocess_input(
+      tf.cast(data, dtype=tf.float32)
+    )
+    """
+    The tf.keras.layers.Lambda function in TensorFlow is used to create
+    a custom layer that applies a given function to the input data. In the provided code snippet,
+    tf.keras.applications.mobilenet.preprocess_input is the function being applied to the data.
+    """
+  ),
+  input_shape = [244,244,3],
+  preTrained_model,
+  tf.keras.layers.GlobalAveragePooling2D(),
+  tf.keras.layers.Dense(256, activation = 'relu'),
+  tf.keras.layers.Dense(2, activation = 'softmax')
+])
+
+mult = 0.4 # for pretrained layers
+mult_by_layer={ 
+            # Clasification head
+            'flower_prob': 1.0,
+            'flower_dense': 1.0,
+            # Pretrained layers
+            'block_1_': 0.02 * mult,
+            'block_2_': 0.04 * mult,
+            'block_3_': 0.06 * mult,
+            'block_4_': 0.08 * mult,
+            'block_5_': 0.1 * mult,
+            'block_6_': 0.15 * mult,
+            'block_7_': 0.2 * mult,
+            'block_8_': 0.25 * mult,
+            'block_9_': 0.3 * mult,
+            'block_10_': 0.35 * mult,
+            'block_11_': 0.4 * mult,
+            'block_12_': 0.5 * mult,
+            'block_13_': 0.6 * mult,
+            'block_14_': 0.7 * mult,
+            'block_15_': 0.8 * mult,
+            'block_16_': 0.9 * mult,
+            # these layers do not have stable identifiers in tf.keras.applications.MobileNetV2
+            'conv': 0.5 * mult,
+            'Conv': 0.5 * mult
+    }
+optimizer = AdamW(lr=0.001, model=model, lr_multipliers=mult_by_layer)
+    
+model.compile(
+    #optimizer='adam',
+    optimizer=optimizer,
+    loss = 'sparse_categorical_crossentropy',
+    metrics=['sparse_categorical_accuracy'],
+    steps_per_execution=8
+)
+model.fit(train_dataset, validation_data=val_dataset, epochs = 10)
